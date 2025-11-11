994fffb085f5141f2ab1091d5e1d9a5c3b08d1b9
vladjantayev-prog <vladjantayev@gmail.com>
2025-11-11 20:10:36 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,58 @@
+window.PRODUCTS = {
+  breakfast: {
+    protein: [
+      {id:"eggs", name:"Яйца", portion:"2 шт", p:12, c:1, f:10, kcal:140},
+      {id:"cottage", name:"Творог 5%", portion:"150 г", p:20, c:6, f:9, kcal:190},
+      {id:"yogurt", name:"Йогурт греч.", portion:"150 г", p:15, c:6, f:4, kcal:120},
+      {id:"turkey", name:"Индейка", portion:"80 г", p:18, c:0, f:2, kcal:100},
+      {id:"tofu", name:"Тоффу", portion:"100 г", p:12, c:3, f:6, kcal:110}
+    ],
+    carbs: [
+      {id:"oats", name:"Овсянка", portion:"50 г сух.", p:6, c:30, f:3, kcal:180},
+      {id:"bread", name:"Хлеб цельноз.", portion:"50 г", p:5, c:22, f:1, kcal:120},
+      {id:"fruit", name:"Фрукты", portion:"150 г", p:1, c:20, f:0, kcal:90}
+    ],
+    fats: [
+      {id:"olive_tsp", name:"Оливковое масло", portion:"1 ч.л.", p:0, c:0, f:5, kcal:45},
+      {id:"nuts", name:"Орехи", portion:"15 г", p:3, c:3, f:10, kcal:110},
+      {id:"avocado", name:"Авокадо", portion:"1/4 шт", p:1, c:3, f:6, kcal:70}
+    ]
+  },
+
+  lunch:   { protein:[{id:"chicken120",name:"Курица",portion:"120 г",p:26,c:0,f:4,kcal:150},
+                      {id:"fish120",name:"Рыба",portion:"120 г",p:23,c:0,f:7,kcal:170},
+                      {id:"turkey120",name:"Индейка",portion:"120 г",p:27,c:0,f:3,kcal:150},
+                      {id:"beef100",name:"Говядина",portion:"100 г",p:23,c:0,f:10,kcal:200}],
+            veg:[{id:"salad200",name:"Свежий салат",portion:"200 г",p:4,c:10,f:0,kcal:60},
+                 {id:"stew200",name:"Тушёные овощи",portion:"200 г",p:5,c:14,f:4,kcal:120},
+                 {id:"baked200",name:"Запечённые овощи",portion:"200 г",p:4,c:18,f:5,kcal:140}],
+            carbs:[{id:"rice120c",name:"Рис бурый",portion:"120 г готового",p:3,c:32,f:1,kcal:150},
+                   {id:"buckwheat120c",name:"Гречка",portion:"120 г готового",p:5,c:25,f:1,kcal:130},
+                   {id:"pasta100c",name:"Паста цельноз.",portion:"100 г готового",p:5,c:30,f:1,kcal:150},
+                   {id:"potato150",name:"Картофель",portion:"150 г",p:3,c:27,f:0,kcal:130}],
+            fats:[{id:"olive_tsp_l",name:"Оливковое масло",portion:"1 ч.л.",p:0,c:0,f:5,kcal:45},
+                  {id:"nuts15",name:"Орехи",portion:"15 г",p:3,c:3,f:10,kcal:110},
+                  {id:"avocado_quarter",name:"Авокадо",portion:"1/4 шт",p:1,c:3,f:6,kcal:70}]},
+
+  dinner:  { protein:[{id:"chicken120_d",name:"Курица",portion:"120 г",p:26,c:0,f:4,kcal:150},
+                      {id:"fish120_d",name:"Рыба",portion:"120 г",p:23,c:0,f:7,kcal:170},
+                      {id:"turkey120_d",name:"Индейка",portion:"120 г",p:27,c:0,f:3,kcal:150},
+                      {id:"eggs2_d",name:"Яйца",portion:"2 шт",p:12,c:1,f:10,kcal:140}],
+            veg:[{id:"big_salad250",name:"Большой салат",portion:"250 г",p:5,c:13,f:0,kcal:75},
+                 {id:"stew250",name:"Тушёные овощи",portion:"250 г",p:6,c:18,f:5,kcal:150},
+                 {id:"soup300",name:"Овощной суп",portion:"300 мл",p:3,c:12,f:3,kcal:95}],
+            carbs:[{id:"rice100c_d",name:"Рис бурый",portion:"100 г готового",p:2,c:27,f:1,kcal:130},
+                   {id:"quinoa80c",name:"Киноа",portion:"80 г готового",p:3,c:20,f:2,kcal:120},
+                   {id:"potato100_d",name:"Картофель",portion:"100 г",p:2,c:18,f:0,kcal:85}],
+            fats:[{id:"olive_tsp_d",name:"Оливковое масло",portion:"1 ч.л.",p:0,c:0,f:5,kcal:45},
+                  {id:"nuts15_d",name:"Орехи",portion:"15 г",p:3,c:3,f:10,kcal:110}]},
+
+  snack:   { items:[
+            {id:"apple_nuts", name:"Яблоко + орехи", portion:"1 шт + 15 г", p:3,c:22,f:10,kcal:180},
+            {id:"yogurt_berries", name:"Йогурт греч. + ягоды", portion:"150 г + 100 г", p:17,c:20,f:3,kcal:180},
+            {id:"veg_hummus", name:"Овощи + хумус", portion:"150 г + 50 г", p:6,c:14,f:7,kcal:140},
+            {id:"bar", name:"Протеиновый батончик", portion:"1 шт", p:20,c:25,f:7,kcal:250},
+            {id:"curd_berries", name:"Творог 5% + ягоды", portion:"100 г + 100 г", p:17,c:10,f:5,kcal:160},
+            {id:"banana_pb", name:"Банан + арах. паста", portion:"1 шт + 1 ч.л.", p:4,c:30,f:5,kcal:190}
+  ]}
+};
